4ae71b3f44f44637c80e47662561a68aff2386d5
BugHunter-X <askzenai@gmail.com>
2025-02-21 22:54:05 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,16 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  elseif x == 0
+    return 0
+  else
+    throw(ArgumentError("Input x must be non-negative. Received: "
+                        * string(x)))
+  end
+end
+
+println(myfunction(2))
+println(myfunction(0))
+println(myfunction(-2))
+```
